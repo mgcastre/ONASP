4698f252b438aa22ccaf6f5d2dd248f40b26d749
Rich Pauloo <richpauloo@gmail.com>
2018-08-06 13:54:34 -0700

    simplications to server logic
    rewrote network plot with ggplotly instead of plotly, which reduced around 90 lines of code to < 10, albeit at the loss of range sliders and other fun things

--- server.R
+++ server.R
@@ -130,94 +130,103 @@ shinyServer(function(input, output, session){
   
   # map for all wells
   output$network <- renderPlotly({
-    temp <- np %>% spread(ID, value)
-    temp <- cbind.data.frame(temp[, -2], temp[, 2]) # re-arrange comentario column to end
+    p <- ggplot(np, aes(Date, value)) + 
+      geom_line(aes(color = ID)) + 
+      geom_smooth(color = "black") + 
+      theme_minimal() + 
+      labs(title = "All monitoring Wells", 
+           y = "Nivel (m)", x = "Fecha")
     
-    p <- qplot(Date, value, data = nr[,-4]) + # remove comentario from plot
-      stat_smooth() 
-    
-    # get geom_smooth coords 
-    ggplot_build(p)$data[[2]] %>% select(x,y,ymin,ymax) -> smooth
-    
-    #smooth$x <- as.Date(as.POSIXct(smooth$x, origin="1970-01-01"))
-    smooth$x <- anytime(smooth$x)
-    
-    # plot 
-    plot_ly(temp, x = ~Date) %>%
-      add_lines(y = ~`PM-01`, name = "PM-01", color= I(pozos_pal[1])) %>%
-      add_lines(y = ~`PM-02`, name = "PM-02", color= I(pozos_pal[2])) %>%
-      add_lines(y = ~`PM-03`, name = "PM-03", color= I(pozos_pal[3])) %>%
-      add_lines(y = ~`PM-04`, name = "PM-04", color= I(pozos_pal[4])) %>%
-      add_lines(y = ~`PM-06`, name = "PM-06", color= I(pozos_pal[5])) %>%
-      add_lines(y = ~`PM-07`, name = "PM-07", color= I(pozos_pal[6])) %>%
-      add_lines(y = ~`PM-08`, name = "PM-08", color= I(pozos_pal[7])) %>%
-      add_lines(y = ~`PM-09`, name = "PM-09", color= I(pozos_pal[8])) %>%
-      add_lines(y = ~`PM-10`, name = "PM-10", color= I(pozos_pal[9])) %>%
-      add_lines(y = ~`PM-12`, name = "PM-12", color= I(pozos_pal[10])) %>%
-      add_lines(y = ~`PM-13`, name = "PM-13", color= I(pozos_pal[11])) %>%
-      add_lines(y = ~`PM-14`, name = "PM-14", color= I(pozos_pal[12])) %>%
-      add_lines(y = ~`PM-18`, name = "PM-18", color= I(pozos_pal[13])) %>%
-      add_lines(y = ~`PM-19`, name = "PM-19", color= I(pozos_pal[14])) %>%
-      add_lines(y = ~`PM-20`, name = "PM-20", color= I(pozos_pal[15])) %>%
-      add_lines(y = ~`PM-21`, name = "PM-21", color= I(pozos_pal[16])) %>%
-      add_lines(y = ~`PM-22`, name = "PM-22", color= I(pozos_pal[17])) %>%
-      add_lines(y = ~`PM-24`, name = "PM-24", color= I(pozos_pal[18])) %>%
-      add_lines(y = ~`PM-25`, name = "PM-25", color= I(pozos_pal[19])) %>%
-      add_lines(y = ~`PM-26`, name = "PM-26", color= I(pozos_pal[20])) %>%
-      add_lines(y = ~`PM-29`, name = "PM-29", color= I(pozos_pal[21])) %>%
-      add_lines(y = ~`PM-31`, name = "PM-31", color= I(pozos_pal[22])) %>%
-      add_lines(y = ~`PM-32`, name = "PM-32", color= I(pozos_pal[23])) %>%
-      add_lines(y = ~`PM-33`, name = "PM-33", color= I(pozos_pal[24])) %>%
-      add_lines(y = ~`PM-36`, name = "PM-36", color= I(pozos_pal[25])) %>%
-      add_lines(y = ~`PM-37`, name = "PM-37", color= I(pozos_pal[26])) %>%
-      add_lines(y = ~`PM-44`, name = "PM-44", color= I(pozos_pal[27])) %>%
-      add_lines(y = ~`PM-60`, name = "PM-60", color= I(pozos_pal[28])) %>%
-      add_lines(y = ~`PM-65`, name = "PM-65", color= I(pozos_pal[29])) %>%
-      add_lines(y = ~`PM-71`, name = "PM-71", color= I(pozos_pal[30])) %>%
-      add_lines(y = ~`PM-73`, name = "PM-73", color= I(pozos_pal[31])) %>%
-      add_lines(y = ~`PM-74`, name = "PM-74", color= I(pozos_pal[32])) %>%
-      add_lines(y = ~`PM-78`, name = "PM-78", color= I(pozos_pal[33])) %>%
-      add_ribbons(data = smooth, x=~x, ymin=~ymin, ymax=~ymax, color = I("darksalmon"), name = "Confidence Interval") %>% 
-      add_lines(data = smooth, x=~x, y=~y, color = I("red"), name = "AVERAGE") %>% 
-      layout(
-        showlegend = FALSE,
-        title = FALSE, #"Entire Monitoring Well Network",
-        xaxis = list(
-          rangeselector = list(
-            buttons = list(
-              list(
-                count = 3,
-                label = "3 mo",
-                step = "month",
-                stepmode = "backward"),
-              list(
-                count = 6,
-                label = "6 mo",
-                step = "month",
-                stepmode = "backward"),
-              list(
-                count = 1,
-                label = "1 yr",
-                step = "year",
-                stepmode = "backward"),
-              list(
-                count = 1,
-                label = "YTD",
-                step = "year",
-                stepmode = "todate"),
-              list(step = "all"))),
-          
-          rangeslider = list(type = "date"),
-          range = c( input$date_range[1], input$date_range[2]) 
-        ),
-        
-        yaxis = list(title = "Nivel (m)")
-        
-      ) %>% 
-      
-      config(displayModeBar = FALSE)
-    
-  })
+    ggplotly(p)
+    })
+  #   temp <- np %>% spread(ID, value)
+  #   temp <- cbind.data.frame(temp[, -2], temp[, 2]) # re-arrange comentario column to end
+  #   
+  #   p <- qplot(Date, value, data = nr[,-4]) + # remove comentario from plot
+  #     stat_smooth() 
+  #   
+  #   # get geom_smooth coords 
+  #   ggplot_build(p)$data[[2]] %>% select(x,y,ymin,ymax) -> smooth
+  #   
+  #   #smooth$x <- as.Date(as.POSIXct(smooth$x, origin="1970-01-01"))
+  #   smooth$x <- anytime(smooth$x)
+  #   
+  #   # plot 
+  #   plot_ly(temp, x = ~Date) %>%
+  #     add_lines(y = ~`PM-01`, name = "PM-01", color= I(pozos_pal[1])) %>%
+  #     add_lines(y = ~`PM-02`, name = "PM-02", color= I(pozos_pal[2])) %>%
+  #     add_lines(y = ~`PM-03`, name = "PM-03", color= I(pozos_pal[3])) %>%
+  #     add_lines(y = ~`PM-04`, name = "PM-04", color= I(pozos_pal[4])) %>%
+  #     add_lines(y = ~`PM-06`, name = "PM-06", color= I(pozos_pal[5])) %>%
+  #     add_lines(y = ~`PM-07`, name = "PM-07", color= I(pozos_pal[6])) %>%
+  #     add_lines(y = ~`PM-08`, name = "PM-08", color= I(pozos_pal[7])) %>%
+  #     add_lines(y = ~`PM-09`, name = "PM-09", color= I(pozos_pal[8])) %>%
+  #     add_lines(y = ~`PM-10`, name = "PM-10", color= I(pozos_pal[9])) %>%
+  #     add_lines(y = ~`PM-12`, name = "PM-12", color= I(pozos_pal[10])) %>%
+  #     add_lines(y = ~`PM-13`, name = "PM-13", color= I(pozos_pal[11])) %>%
+  #     add_lines(y = ~`PM-14`, name = "PM-14", color= I(pozos_pal[12])) %>%
+  #     add_lines(y = ~`PM-18`, name = "PM-18", color= I(pozos_pal[13])) %>%
+  #     add_lines(y = ~`PM-19`, name = "PM-19", color= I(pozos_pal[14])) %>%
+  #     add_lines(y = ~`PM-20`, name = "PM-20", color= I(pozos_pal[15])) %>%
+  #     add_lines(y = ~`PM-21`, name = "PM-21", color= I(pozos_pal[16])) %>%
+  #     add_lines(y = ~`PM-22`, name = "PM-22", color= I(pozos_pal[17])) %>%
+  #     add_lines(y = ~`PM-24`, name = "PM-24", color= I(pozos_pal[18])) %>%
+  #     add_lines(y = ~`PM-25`, name = "PM-25", color= I(pozos_pal[19])) %>%
+  #     add_lines(y = ~`PM-26`, name = "PM-26", color= I(pozos_pal[20])) %>%
+  #     add_lines(y = ~`PM-29`, name = "PM-29", color= I(pozos_pal[21])) %>%
+  #     add_lines(y = ~`PM-31`, name = "PM-31", color= I(pozos_pal[22])) %>%
+  #     add_lines(y = ~`PM-32`, name = "PM-32", color= I(pozos_pal[23])) %>%
+  #     add_lines(y = ~`PM-33`, name = "PM-33", color= I(pozos_pal[24])) %>%
+  #     add_lines(y = ~`PM-36`, name = "PM-36", color= I(pozos_pal[25])) %>%
+  #     add_lines(y = ~`PM-37`, name = "PM-37", color= I(pozos_pal[26])) %>%
+  #     add_lines(y = ~`PM-44`, name = "PM-44", color= I(pozos_pal[27])) %>%
+  #     add_lines(y = ~`PM-60`, name = "PM-60", color= I(pozos_pal[28])) %>%
+  #     add_lines(y = ~`PM-65`, name = "PM-65", color= I(pozos_pal[29])) %>%
+  #     add_lines(y = ~`PM-71`, name = "PM-71", color= I(pozos_pal[30])) %>%
+  #     add_lines(y = ~`PM-73`, name = "PM-73", color= I(pozos_pal[31])) %>%
+  #     add_lines(y = ~`PM-74`, name = "PM-74", color= I(pozos_pal[32])) %>%
+  #     add_lines(y = ~`PM-78`, name = "PM-78", color= I(pozos_pal[33])) %>%
+  #     add_ribbons(data = smooth, x=~x, ymin=~ymin, ymax=~ymax, color = I("darksalmon"), name = "Confidence Interval") %>% 
+  #     add_lines(data = smooth, x=~x, y=~y, color = I("red"), name = "AVERAGE") %>% 
+  #     layout(
+  #       showlegend = FALSE,
+  #       title = FALSE, #"Entire Monitoring Well Network",
+  #       xaxis = list(
+  #         rangeselector = list(
+  #           buttons = list(
+  #             list(
+  #               count = 3,
+  #               label = "3 mo",
+  #               step = "month",
+  #               stepmode = "backward"),
+  #             list(
+  #               count = 6,
+  #               label = "6 mo",
+  #               step = "month",
+  #               stepmode = "backward"),
+  #             list(
+  #               count = 1,
+  #               label = "1 yr",
+  #               step = "year",
+  #               stepmode = "backward"),
+  #             list(
+  #               count = 1,
+  #               label = "YTD",
+  #               step = "year",
+  #               stepmode = "todate"),
+  #             list(step = "all"))),
+  #         
+  #         rangeslider = list(type = "date"),
+  #         range = c( input$date_range[1], input$date_range[2]) 
+  #       ),
+  #       
+  #       yaxis = list(title = "Nivel (m)")
+  #       
+  #     ) %>% 
+  #     
+  #     config(displayModeBar = FALSE)
+  #   
+  # })
   
   # Download All Data
   output$download_all_data <- downloadHandler(
