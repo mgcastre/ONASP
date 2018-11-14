dad07d4b8d8e7034d60fbe036af2cd77f4ff039a
Maria Gabriela Castrellon <mgcastrellon@ucdavis.edu>
2018-11-13 23:37:37 -0500

    changed shinytheme and updated distribution of elements in the page

--- ui.R
+++ ui.R
@@ -1,63 +1,36 @@
-shinyUI(navbarPage(theme=shinytheme("spacelab"), 
-                   title=HTML('<div><a href="http://ucwater.org/" target="_blank"><img src="./img/small_logo.png" width="80%"></a></div>'),
-                   tabPanel("Pozos", value="commChart"),
-                   tabPanel("Network", value="network"),
-                   tabPanel("About", value="about"),
-                   windowTitle="UC Water | Groundwater Observatory",
-                   collapsible=TRUE,
+shinyUI(fluidPage(theme = shinytheme("cerulean"),
+                  titlePanel("ONAS: Observatorio Nacional de Aguas Subterráneas"),
+                  tabsetPanel(
+                   tabPanel("Mapa Interactivo", value="map"),
+                   tabPanel("Datos", value="network"),
+                   tabPanel("Más Información", value="about"),
                    id="tsp",
-                   tags$head(includeScript("www/ga-cc4liteFinal.js"), includeScript("www/ga-allapps.js")),
-                   tags$head(tags$link(rel="stylesheet", type="text/css", href="styles.css")),
                    
                    # chart panel
-                   conditionalPanel("input.tsp=='commChart'",
+                   conditionalPanel(condition = "input.tsp=='map'",
                                     
                                     # row 1
-                                    fluidRow(
-                                      column(4, 
-                                             h4("California Groundwater Observatory"), 
-                                             h6("Real-time aquifer monitoring in the South American Subbasin")),
-                                      column(8,
-                                             fluidRow(
-                                               column(6, 
-                                                      selectInput("location", "Monitoring Well ID", 
-                                                                  c("", unique(np$ID)), 
-                                                                  selected="", multiple=F, width="100%"))
-                                             )
-                                      )
-                                    ),
-                                    bsTooltip("location", "Enter a monitoring well ID. The menu will filter as you type. You may also select a monitoring well using the map.", "top", options = list(container="body")),
+                                    fluidRow(column(6, leafletOutput("Map")),
+                                    column(6, offset = 6,
+                                           fluidRow(column(12, 
+                                                     selectInput("location", "Seleccionar Pozo", 
+                                                                c("", unique(np$ID)), 
+                                                                selected="", multiple=F, width="100%")),
+                                                    column(12, plotlyOutput("Chart1"))
+                                    )),
                                     
                                     # row 2
-                                    fluidRow(
-                                      column(4, 
-                                             leafletOutput("Map")),
-                                      column(8,
-                                             plotlyOutput("Chart1"),
-                                             HTML('<style>.rChart {width: 100%; height: "auto"}</style>'))
-                                    ),
-                                    br(),
-                                    
-                                    # row 3
                                     fluidRow(
                                       column(2, actionButton("help_loc_btn", "Site Info", class="btn-block"), br()),
                                       column(2, actionButton("help_rcp_btn", "Recharge", class="btn-block")),
                                       column(7, h5(HTML(paste(caption, '<a href="http://ucwater.org/" target="_blank">ucwater.org</a>'))))
-                                    ),
-                                    bsModal("modal_loc", "Site Information", "help_loc_btn", size="large",
-                                            includeMarkdown("data/site_info.md")),
-                                    
-                                    bsModal("modal_rcp", "Floodplain Recharge", "help_rcp_btn", size="large",
-                                            includeMarkdown("data/recharge_info.md"))
+                                    )
                    ),
                    
                    
                    # network panel	
                    conditionalPanel("input.tsp=='network'", 
                                     fluidRow(
-                                      column(4, 
-                                             h4("California Groundwater Observatory"), 
-                                             h6("Real-time aquifer monitoring in the South American Subbasin")),
                                       column(8,
                                              fluidRow(
                                                column(6, 
@@ -82,6 +55,5 @@ shinyUI(navbarPage(theme=shinytheme("spacelab"),
                    
                    
                    # about panel
-                   conditionalPanel("input.tsp=='about'", includeMarkdown("data/about.md")) #source("about.R",local=T)$value)
-                   
-))
+                   conditionalPanel("input.tsp=='about' ")
+))))
