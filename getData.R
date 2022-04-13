147efe60aaeff49a38043f92d3668ce225f3b3b1
Maria Gabriela Castrellon <mariag.castrellon@gmail.com>
2022-04-13 00:52:57 -0500

    Added read precipitation to getData

--- getData.R
+++ getData.R
@@ -15,31 +15,65 @@ utils::download.file(url=file.path(url), destfile=sqlFile, method="libcurl", mod
 edb <- dbConnect(SQLite(),dbname=sqlFile, synchronous=NULL)
 Head_Data <- dbReadTable(edb, "Head_Data")
 Head_Points <- dbReadTable(edb, "Head_Points")
+rglocs <- dbReadTable(objDB, "Precipitation_Locations")
+rgdata <- dbReadTable(objDB, "Precipitation_Measurements")
 dbDisconnect(edb)
 
-## Select well points and data
-wells <- filter(Head_Points, Type == "well")
+## Select Well Points
+wells <- filter(Head_Points, Type == "well") %>% mutate(ID = as.factor(ID))
+
+## Select GW Data from Wells
 wdata <- Head_Data %>% 
   filter(str_detect(ID, "PM") | str_detect(ID,"MP")) %>% 
   mutate(DTW = -1*DTW, Date = ymd(Date), ID = as.factor(ID))
 
-## Create spatial well points
+## Generate Monthly Precipitation Measurements
+rgdata_monthly <- rgdata %>% 
+  mutate(Month = month(Date), 
+         Year = year(Date),
+         ID = as.factor(ID)) %>% 
+  group_by(ID, Year, Month) %>% 
+  summarize(Value = sum(Value)) %>% 
+  ungroup() %>% 
+  mutate(Date = ymd(paste(Year,Month,"01",sep="-")),
+         Unit = "mm/month") %>% 
+  dplyr::select(ID, Date, Value, Unit)
+
+## Define coordinate reference systems
 crs32617 <- CRS("+proj=utm +zone=17 +ellps=WGS84 +datum=WGS84 +units=m +no_defs")
+crs4326 <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84")
+
+## Create Spatial Well Points
 wsp <- SpatialPointsDataFrame(
   coords = dplyr::select(wells, X, Y), 
   data   = dplyr::select(wells, ID, Z:LTP),
   proj4string = crs32617)
 
-## transform coordinates for wells
-crs4326 <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84")
-wsp_ll <- spTransform(wsp, crs4326)
+## Create Spatial Rain Gages
+rgsp <- SpatialPointsDataFrame(
+  coords = dplyr::select(rglocs, X, Y), 
+  data   = dplyr::select(rglocs, ID, LoggerID, Province:Project),
+  proj4string = crs32617)
 
-## Add labesl for wells
+## Transform Coordinates to Lat Lon
+wsp_ll <- spTransform(wsp, crs4326)
+rgsp_ll <- spTransform(rgsp, crs4326)
+
+## Add Labels for Wells
 wsp_ll$New_Name <- 
   paste0('<strong>', wsp_ll$ID, '</strong>',
-         '<br/>', 'Distrito: ', wsp_ll$District,
-         '<br/>', 'Corregimiento: ', wsp_ll$Township, 
-         '<br/>', 'Localidad: ', wsp_ll$Location) %>% 
+         '<br/>', 'District: ', wsp_ll$District,
+         '<br/>', 'Township: ', wsp_ll$Township, 
+         '<br/>', 'Location: ', wsp_ll$Location,
+         '<br/>', 'Depth: ', wsp_ll$Depth, ' m') %>% 
+  lapply(htmltools::HTML)
+
+## Add Labels for Rain Gages
+rgsp_ll$New_Name <- 
+  paste0('<strong>', rgsp_ll$ID, '</strong>',
+         '<br/>', 'District: ', rgsp_ll$District,
+         '<br/>', 'Townsip: ', rgsp_ll$Township, 
+         '<br/>', 'Location: ', rgsp_ll$Location) %>% 
   lapply(htmltools::HTML)
 
 # 2. Download GeoJSON files from Dropbox =======================================
